f451751aa7f4e62407eb340df56b8a60fbe98918
edom44 <edomiax94@gmail.com>
2018-06-12 17:50:28 +0000

    validate user with the article

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+has_many :articles
 
+before_save { self.email = eamil.downcase }
 validates :username, presence: true,
 
         uniqueness: { case_sensitive: false },
